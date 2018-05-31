38b3fe9d410dc55b3814f6be42f337faeeb2a1df
Nick Saunders <nick.saunders@experian.com>
2018-05-31 08:49:18 -0700

    Demonstrate how to access connection/components in a resource function

--- examples/RoutingReaderT.purs
+++ examples/RoutingReaderT.purs
@@ -1,16 +1,21 @@
 module Examples.RoutingReaderT where
 
 import Prelude
-import Control.IxMonad ((:*>))
+import Control.IxMonad ((:*>), (:>>=))
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Except (ExceptT)
 import Control.Monad.Reader (ReaderT, ask, runReaderT)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Tuple (Tuple(..))
+import Hyper.Conn (Conn)
+import Hyper.Middleware.Class (getConn)
+import Hyper.Node.BasicAuth as BasicAuth
 import Hyper.Node.Server (defaultOptionsWithLogging, runServer')
 import Hyper.Response (closeHeaders, respond, writeStatus)
 import Hyper.Trout.Router (RoutingError, router)
+import Node.Buffer (BUFFER)
 import Node.HTTP (HTTP)
 import Text.Smolder.HTML (p)
 import Text.Smolder.Markup (text)
@@ -33,19 +38,34 @@ site :: Proxy Site
 site = Proxy
 
 greetingResource
-  :: forall m
+  :: forall m req res t
    . Monad m
-  => {"GET" :: ExceptT RoutingError (ReaderT String m) Greeting}
-greetingResource =
-  {"GET": Greeting <$> ask}
+  => Show t
+  => Conn req res { authentication :: Maybe t }
+  -> {"GET" :: ExceptT RoutingError (ReaderT String m) Greeting}
+greetingResource conn =
+  {"GET": (Greeting <<< (flip append) (fromMaybe "Guest" (show <$> conn.components.authentication))) <$> ask}
 
-main :: forall e. Eff (console :: CONSOLE, http :: HTTP | e) Unit
+data User = User String
+
+instance showUser :: Show User where
+  show (User name) = name
+
+-- This could be a function checking the username/password in a database.
+userFromBasicAuth :: forall e. Tuple String String -> ReaderT String (Aff e) (Maybe User)
+userFromBasicAuth =
+  case _ of
+    Tuple "admin" "admin" -> pure (Just (User "Administrator"))
+    _ -> pure Nothing
+
+main :: forall e. Eff (buffer :: BUFFER, console :: CONSOLE, http :: HTTP | e) Unit
 main =
-  let app = router site {"greeting": greetingResource} onRoutingError
+  let app = BasicAuth.withAuthentication userFromBasicAuth
+            :*> getConn :>>= \conn -> router site {"greeting": (greetingResource conn)} onRoutingError
 
       onRoutingError status msg =
         writeStatus status
         :*> closeHeaders
         :*> respond (fromMaybe "" msg)
 
-  in runServer' defaultOptionsWithLogging {} (runAppM "Hello") app
+  in runServer' defaultOptionsWithLogging { authentication: unit } (runAppM "Hello, ") app
